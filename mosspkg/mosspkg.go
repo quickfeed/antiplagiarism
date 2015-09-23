731eb7c44ed86ca8ca0b9c762be02ff79726b063
Hein Meling <hein.meling@uis.no>
2015-09-23 16:45:11 +0200

    refactor: revised doc formating and a few other things; see below.
    golint complains about doc formating that does not follow the go
    convention. golint also complains about some of the declariations
    of variables that specify type on both sides etc.
    
    moreover, we should avoid using the variable name 'error' since
    that is also a keyword in go.
    
    i have also replaced some instances of + "/" + with OS-friendly
    std lib code, instead using filepath.Join().

--- mosspkg/mosspkg.go
+++ mosspkg/mosspkg.go
@@ -3,12 +3,14 @@ package mosspkg
 import (
 	"bytes"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type matches struct {
@@ -28,37 +30,42 @@ type matches struct {
 // Output: The 2d slice of directories
 //		Whether or not the function was successful.
 //***************************************************************************
-func DirectoryContents(baseDir string, labs []LabInfo) ([][]string, bool) {
-	var labsCount int = len(labs)
-	var studentCount int
 
+// DirectoryContents returns a two-dimensional slice with full-path directories
+// to send to MOSS for evaluation. The first index addresses the specific lab,
+// and the second index addresses the specific student. If a student does not
+// have the lab directory, the 2D slice will save the directory as an empty
+// string. The second return argument indicates whether or not the function was
+// successful. DirectoryContents takes as input baseDir, the location of the
+// student directories, and labs, a slice of the labs.
+func DirectoryContents(baseDir string, labs []LabInfo) ([][]string, bool) {
 	// Try to read the base directory
-	contents, error := ioutil.ReadDir(baseDir)
-	if error != nil {
-		fmt.Printf("Error reading directory %s: %s\n", baseDir, error)
+	contents, err := ioutil.ReadDir(baseDir)
+	if err != nil {
+		fmt.Printf("Error reading directory %s: %s\n", baseDir, err)
 		return nil, false
 	}
 
 	var studentDirs []string
-
 	// Get a list of all the student directories (full path)
 	for _, item := range contents {
 		if item.IsDir() {
-			studentDirs = append(studentDirs, baseDir+"/"+item.Name())
+			studentDirs = append(studentDirs, filepath.Join(baseDir, item.Name()))
 		}
 	}
-	studentCount = len(studentDirs)
 
-	var studentsLabDirs [][]string = make([][]string, labsCount)
+	studentsLabDirs := make([][]string, len(labs))
 	// For each lab
 	for i := range studentsLabDirs {
-		studentsLabDirs[i] = make([]string, studentCount)
+		studentsLabDirs[i] = make([]string, len(studentDirs))
 
 		// For each student
 		for j := range studentsLabDirs[i] {
-			tempDir := studentDirs[j] + "/" + labs[i].Name + "/"
-			_, error := ioutil.ReadDir(tempDir)
-			if error != nil {
+			// tempDir := studentDirs[j] + "/" + labs[i].Name + "/"
+			//TODO: Not sure if the last "/" is important above; the filepath.Join() probably won't be able to add a "/" at the end.
+			tempDir := filepath.Join(studentDirs[j], labs[i].Name)
+			_, err := ioutil.ReadDir(tempDir)
+			if err != nil {
 				studentsLabDirs[i][j] = ""
 			} else {
 				studentsLabDirs[i][j] = tempDir
