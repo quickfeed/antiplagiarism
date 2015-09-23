c6d65c4a8d0320c5273507863ab8152c26e58976
Eric Freeman <ericfree@badne7.ux.uis.no>
2015-09-23 15:50:29 +0200

    Format

--- mosspkg/mosspkg.go
+++ mosspkg/mosspkg.go
@@ -3,20 +3,19 @@ package mosspkg
 import (
 	"bytes"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
-	"github.com/PuerkitoBio/goquery"
 )
 
 type matches struct {
-	url string
+	url        string
 	match1text string
 	match2text string
 }
-
 
 //***************************************************************************
 // This function will create a two-dimensional slice containing the
@@ -70,7 +69,6 @@ func DirectoryContents(baseDir string, labs []LabInfo) ([][]string, bool) {
 	return studentsLabDirs, true
 }
 
-
 //***************************************************************************
 // This function will create MOSS commands to upload the lab files.
 // Input: mossDir - the location of the MOSS script
@@ -118,7 +116,7 @@ func CreateMossCommands(mossDir string, studentsLabDirs [][]string, labs []LabIn
 				}
 			}
 		}
-		
+
 		buf.WriteString(" > " + labs[i].Name + ".txt &")
 
 		// Add the MOSS command for this lab
@@ -127,7 +125,6 @@ func CreateMossCommands(mossDir string, studentsLabDirs [][]string, labs []LabIn
 
 	return commands, true
 }
-
 
 //***************************************************************************
 // This function saves the data from the specified MOSS URL
@@ -141,89 +138,88 @@ func SaveMossResults(url string, baseDir string, lab LabInfo) bool {
 
 	os.MkdirAll(resultsDir, 0764)
 	os.Remove(resultsDir + "*.*")
-	
+
 	var comparisons []matches
-	
+
 	// Get web page data
-	doc, err := goquery.NewDocument(url) 
-  if err != nil {
-  	fmt.Printf("%v\n", err)
-    return false
-  }
-  
-  // Find the table rows
-  doc.Find("tr").Each(func(i int, tr *goquery.Selection) {
-  	var match matches
-  
-  	// Find the table columns
-  	tr.Find("td").Each(func(j int, td *goquery.Selection) {
-  		url := ""
-  
-  		// Find the href attributes
-  		td.Find("a").Each(func(k int, a *goquery.Selection) {
-  			url, _ = a.Attr("href")
-  		})
-  	
-  		// If there is an href attribute
-  		if url != "" {
-  			match.url = url
-  			val := td.Text()
-  			if match.match1text == "" {
-  				match.match1text = val
-  			} else {
-  				match.match2text = val
-  			}
-  		}
-  	})
-  	
-  	if match.url != "" {
-  		comparisons = append(comparisons, match)
-  	}
-  })
-	
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return false
+	}
+
+	// Find the table rows
+	doc.Find("tr").Each(func(i int, tr *goquery.Selection) {
+		var match matches
+
+		// Find the table columns
+		tr.Find("td").Each(func(j int, td *goquery.Selection) {
+			url := ""
+
+			// Find the href attributes
+			td.Find("a").Each(func(k int, a *goquery.Selection) {
+				url, _ = a.Attr("href")
+			})
+
+			// If there is an href attribute
+			if url != "" {
+				match.url = url
+				val := td.Text()
+				if match.match1text == "" {
+					match.match1text = val
+				} else {
+					match.match2text = val
+				}
+			}
+		})
+
+		if match.url != "" {
+			comparisons = append(comparisons, match)
+		}
+	})
+
 	// For each URL
 	for _, match := range comparisons {
 		pos1 := strings.LastIndex(match.url, "/")
 		pos2 := strings.LastIndex(match.url, ".html")
-		base := match.url[pos1 + 1: pos2]
+		base := match.url[pos1+1 : pos2]
 		topFrame := strings.Replace(match.url, ".html", "-top.html", 1)
-  	leftFrame := strings.Replace(match.url, ".html", "-0.html", 1)
-  	rightFrame := strings.Replace(match.url, ".html", "-1.html", 1)
-	
+		leftFrame := strings.Replace(match.url, ".html", "-0.html", 1)
+		rightFrame := strings.Replace(match.url, ".html", "-1.html", 1)
+
 		// Get and save web page data
 		linkBody, success := GetHtmlData(match.url)
 		if !success {
 			return false
 		}
-		ioutil.WriteFile(resultsDir + base + ".html", []byte(linkBody), 0644)
-		
+		ioutil.WriteFile(resultsDir+base+".html", []byte(linkBody), 0644)
+
 		// Get and save top frame
 		topBody, success := GetHtmlData(topFrame)
 		if !success {
 			return false
 		}
-		ioutil.WriteFile(resultsDir + base + "-top.html", []byte(topBody), 0644)
-		
+		ioutil.WriteFile(resultsDir+base+"-top.html", []byte(topBody), 0644)
+
 		// Get and save left frame
 		leftBody, success := GetHtmlData(leftFrame)
 		if !success {
 			return false
 		}
-		ioutil.WriteFile(resultsDir + base + "-0.html", []byte(leftBody), 0644)
-		
+		ioutil.WriteFile(resultsDir+base+"-0.html", []byte(leftBody), 0644)
+
 		// Get and save right frame
 		rightBody, success := GetHtmlData(rightFrame)
 		if !success {
 			return false
 		}
-		ioutil.WriteFile(resultsDir + base + "-1.html", []byte(rightBody), 0644)
+		ioutil.WriteFile(resultsDir+base+"-1.html", []byte(rightBody), 0644)
 	}
-	
+
 	MakeResultsMainPage(resultsDir, lab, comparisons)
-		
+
 	return true
 }
-
 
 //***************************************************************************
 // This function returns html data as a string
@@ -235,11 +231,11 @@ func GetHtmlData(url string) (string, bool) {
 	var body string = ""
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
-	
+
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return "", false
-	} else if (resp.StatusCode != 200) {
+	} else if resp.StatusCode != 200 {
 		fmt.Printf("%v\n", resp.Status)
 		return "", false
 	} else {
@@ -247,14 +243,13 @@ func GetHtmlData(url string) (string, bool) {
 		if err != nil {
 			fmt.Printf("%v\n", err)
 			return "", false
-		}		
-		
+		}
+
 		body = string(bodyBuf)
 	}
-	
+
 	return body, true
 }
-
 
 //***************************************************************************
 // This function creates the main html file for the results
@@ -272,8 +267,8 @@ func MakeResultsMainPage(resultsDir string, lab LabInfo, comparisons []matches) 
 	buf.WriteString(lab.Name + " Results<br>")
 	for _, match := range comparisons {
 		pos := strings.LastIndex(match.url, "/")
-		base := match.url[pos + 1:]
-		
+		base := match.url[pos+1:]
+
 		buf.WriteString("\n<A HREF=\"")
 		buf.WriteString(base)
 		buf.WriteString("\">")
@@ -281,6 +276,6 @@ func MakeResultsMainPage(resultsDir string, lab LabInfo, comparisons []matches) 
 		buf.WriteString("</A><br>")
 	}
 	buf.WriteString("\n</BODY>\n</HTML>\n")
-	
-	ioutil.WriteFile(resultsDir + "results.html", []byte(buf.String()), 0644)
+
+	ioutil.WriteFile(resultsDir+"results.html", []byte(buf.String()), 0644)
 }
