8daf80130ed5f9520f61ecf11f26f95e27b86f74
Eric Freeman <ericfree@badne7.ux.uis.no>
2015-10-20 19:26:40 +0200

    Put parsing in new file. Split commands and results functionality.

--- antiplagiarism.go
+++ antiplagiarism.go
@@ -5,55 +5,16 @@ import (
 	"./dupl"
 	"./jplag"
 	"./moss"
-	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
-)
-
-var (
-	help = flag.Bool(
-		"help",
-		false,
-		"Show usage help",
-	)
-	token = flag.String(
-		"token",
-		"",
-		"The oauth admin token which authorizes this application to connect to github",
-	)
-	mainrepo = flag.String(
-		"mainrepo",
-		"",
-		"The main github repository",
-	)
-	repos = flag.String(
-		"repos",
-		"",
-		"The list of student repositories, delimited by commas",
-	)
-	labs = flag.String(
-		"labs",
-		"",
-		"The list of labs to check, delimited by commas",
-	)
-	languages = flag.String(
-		"languages",
-		"",
-		"The list of languages of the labs, delimited by commas. 0 = Java, 1 = Go, 2 = C++",
-	)
 )
 
 var studentRepos []string
 var labNames []string
 var labLanguages []int
-
-func usage() {
-	flag.PrintDefaults()
-	fmt.Printf("Example: ./antiplagiarism -token=0123456789ABCDEF -mainrepo=DAT320 ")
-	fmt.Printf("-repos=Student1,Student2,Student3 -labs=LabA,LabB,LabC -languages=0,1,0\n")
-}
+var getResults bool
+var mainRepository string
+var githubToken string
 
 func main() {
 	if !ReadConfig() {
@@ -65,6 +26,27 @@ func main() {
 		os.Exit(0)
 	}
 
+	if !getResults {
+		buildAndRunCommands()
+	} else {
+		checkAndStoreResults()
+	}
+}
+
+// buildLabInfo() creates the LabInfo structures from the command
+// line arguments to give to the various antiplagiarism packages.
+// The return argument is the slice of LabInfo structs.
+func buildLabInfo() []common.LabInfo {
+	var labInfo []common.LabInfo
+
+	for i := range labNames {
+		labInfo = append(labInfo, common.LabInfo{labNames[i], mainRepository, labLanguages[i]})
+	}
+
+	return labInfo
+}
+
+func buildAndRunCommands() bool {
 	// TODO: Download files from github using oath token from Autograder
 
 	labInfo := buildLabInfo()
@@ -98,73 +80,13 @@ func main() {
 
 	// TODO: Run commands
 
-	// TODO: Store results
-}
-
-// parseArgs() parses the command line arguments.
-// The return argument indicates whether or not the function was successful.
-func parseArgs() bool {
-	flag.Usage = usage
-	flag.Parse()
-	if *help {
-		flag.Usage()
-		return false
-	}
-
-	if *token == "" {
-		fmt.Printf("No token provided.\n")
-		return false
-	}
-
-	if *mainrepo == "" {
-		fmt.Printf("No main repository provided.\n")
-		return false
-	}
-
-	if *repos == "" {
-		fmt.Printf("No student repositories provided.\n")
-		return false
-	}
-	studentRepos = strings.Split(*repos, ",")
-
-	if *labs == "" {
-		fmt.Printf("No lab names provided.\n")
-		return false
-	}
-	labNames = strings.Split(*labs, ",")
-
-	if *languages == "" {
-		fmt.Printf("No languages provided.\n")
-		return false
-	}
-	langStr := strings.Split(*languages, ",")
-
-	if len(labNames) != len(langStr) {
-		fmt.Printf("The number of labs does not equal the number of languages provided.\n")
-		return false
-	}
-
-	for _, lang := range langStr {
-		langNum, err := strconv.Atoi(lang)
-		if err != nil {
-			fmt.Printf("Error parsing languages: %v.\n", err)
-			return false
-		}
-		labLanguages = append(labLanguages, langNum)
-	}
-
 	return true
 }
 
-// buildLabInfo() creates the LabInfo structures from the command
-// line arguments to give to the various antiplagiarism packages.
-// The return argument is the slice of LabInfo structs.
-func buildLabInfo() []common.LabInfo {
-	var labInfo []common.LabInfo
+func checkAndStoreResults() bool {
+	// TODO: Check if results are there
 
-	for i := range labNames {
-		labInfo = append(labInfo, common.LabInfo{labNames[i], *mainrepo, labLanguages[i]})
-	}
+	// TODO: Store results
 
-	return labInfo
+	return true
 }
---
+++ parseArgs.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var (
+	help = flag.Bool(
+		"help",
+		false,
+		"Show usage help",
+	)
+	results = flag.Bool(
+		"results",
+		false,
+		"Check and retrieve the results from the various anti-plagiarism applications",
+	)
+	token = flag.String(
+		"token",
+		"",
+		"The oauth admin token which authorizes this application to connect to github",
+	)
+	mainrepo = flag.String(
+		"mainrepo",
+		"",
+		"The main github repository",
+	)
+	repos = flag.String(
+		"repos",
+		"",
+		"The list of student repositories, delimited by commas",
+	)
+	labs = flag.String(
+		"labs",
+		"",
+		"The list of labs to check, delimited by commas",
+	)
+	languages = flag.String(
+		"languages",
+		"",
+		"The list of languages of the labs, delimited by commas. 0 = Java, 1 = Go, 2 = C++",
+	)
+)
+
+func usage() {
+	flag.PrintDefaults()
+	fmt.Printf("Example: ./antiplagiarism -token=0123456789ABCDEF -mainrepo=DAT320 ")
+	fmt.Printf("-repos=Student1,Student2,Student3 -labs=LabA,LabB,LabC -languages=0,1,0\n")
+	fmt.Printf("Example: ./antiplagiarism -results -mainrepo=DAT320 -labs=LabA,LabB,LabC\n")
+}
+
+// parseArgs() parses the command line arguments.
+// The return argument indicates whether or not the function was successful.
+func parseArgs() bool {
+	flag.Usage = usage
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		return false
+	}
+
+	if *results {
+		getResults = true
+	} else {
+		getResults = false
+	}
+
+	if *mainrepo == "" {
+		fmt.Printf("No main repository provided.\n")
+		return false
+	}
+	mainRepository = *mainrepo
+
+	if *labs == "" {
+		fmt.Printf("No lab names provided.\n")
+		return false
+	}
+	labNames = strings.Split(*labs, ",")
+
+	if !getResults {
+		if *token == "" {
+			fmt.Printf("No token provided.\n")
+			return false
+		}
+		githubToken = *token
+
+		if *repos == "" {
+			fmt.Printf("No student repositories provided.\n")
+			return false
+		}
+		studentRepos = strings.Split(*repos, ",")
+
+		if *languages == "" {
+			fmt.Printf("No languages provided.\n")
+			return false
+		}
+		langStr := strings.Split(*languages, ",")
+
+		if len(labNames) != len(langStr) {
+			fmt.Printf("The number of labs does not equal the number of languages provided.\n")
+			return false
+		}
+
+		for _, lang := range langStr {
+			langNum, err := strconv.Atoi(lang)
+			if err != nil {
+				fmt.Printf("Error parsing languages: %v.\n", err)
+				return false
+			}
+			labLanguages = append(labLanguages, langNum)
+		}
+	}
+
+	return true
+}
