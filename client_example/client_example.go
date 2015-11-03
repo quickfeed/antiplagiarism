3da229e9fe49096c059a6f0d301ee8cd2db40aff
Eric Freeman <ericfree@badne7.ux.uis.no>
2015-11-03 12:48:20 +0100

    Added sample client.

---
+++ client_example/client_example.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "github.com/autograde/antiplagiarism/proto"
+)
+
+// The language the lab was written in
+const (
+	Java = iota
+	Golang
+	Cpp
+	C
+)
+
+var (
+	help = flag.Bool(
+		"help",
+		false,
+		"Show usage help",
+	)
+	endpoint = flag.String(
+		"endpoint",
+		"localhost:12111",
+		"Endpoint on which server runs or to which client connects",
+	)
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	// Create connection
+	conn, err := grpc.Dial(*endpoint)
+	if err != nil {
+		fmt.Printf("Error while connecting to server: %v\n", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Printf("Connected to server on %v\n", *endpoint)
+
+	// Create client
+	client := pb.NewApClient(conn)
+
+	// Create request
+	request := pb.ApRequest{GithubOrg: "ag2-test",
+		GithubToken:  "12345",
+		StudentRepos: []string{"ericnorway-labs", "ericnorwaytest-labs"},
+		LabNames:     []string{"lab1", "lab2"},
+		LabLanguages: []int32{C, Golang}}
+
+	// Send request and get response
+	response, err := client.CheckPlagiarism(context.Background(), &request)
+
+	// Check response
+	if err != nil {
+		fmt.Printf("gRPC error: %s\n", err)
+	} else if response.Success == false {
+		fmt.Printf("Anti-plagiarism error: %s\n", response.Err)
+	} else {
+		fmt.Printf("Anti-plagiarism application ran successfully.\n")
+	}
+}
